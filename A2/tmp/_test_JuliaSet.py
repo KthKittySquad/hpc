e09494697654f548bea057d45eaa5cd2c0820f63
Albin Wallenius Woxnerud <albin@woxnerud.se>
2025-01-23 21:52:17 +0100

    alas I forgor this

--- A2/tmp/_test_JuliaSet.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from JuliaSet import calc_pure_python
-
-
-@pytest.mark.parametrize('width, iterations, expected', [(1000, 300, 33219980), (10, 3, 177)])
-def test_calc_pure_python(width, iterations, expected):
-    output = calc_pure_python(width, iterations)
-    assert sum(output) == expected
